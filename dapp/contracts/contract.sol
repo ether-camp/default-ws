6b59564f9ff9ad758719233f2d19a460dfecc4a6
Alexander Sinyagin <sinyagin.alexander@gmail.com>
2015-10-06 16:22:09 +0000

    Workspace for testing

--- dapp/contracts/contract.sol
+++ dapp/contracts/contract.sol
@@ -1,7 +1,8 @@
 import "std.sol";
 
 contract Contract is named("Contract") {
-    function Contract() {
-        log0("hello from the contract");
+    uint i1;
+    function test(uint i) {
+        i1 = i;
     }
 }
